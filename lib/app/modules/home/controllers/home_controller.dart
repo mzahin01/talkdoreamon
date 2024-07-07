f76a427edbac9ece2ff0285b9af5f5629c9a9457
mzahin01 <minhazahin@gmail.com>
2024-07-07 19:02:11 +0600

    doremon running

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,12 +2,35 @@ import 'package:get/get.dart';
 import 'package:rive/rive.dart';
 
 class HomeController extends GetxController {
+  StateMachineController? _controller;
+  SMIInput<bool>? _speakInput;
+  SMIInput<bool>? _sAndOInput;
+  SMIInput<bool>? _thoughtInput;
+  SMIInput<bool>? _hiDoremonInput;
+  SMIInput<bool>? _sunnahSmileInput;
+
   void onRiveInit(Artboard artboard) {
     final controller = StateMachineController.fromArtboard(
       artboard,
       'State Machine 1',
-      // onStateChange: _onStateChange,
     );
-    artboard.addController(controller!);
+    if (controller != null) {
+      artboard.addController(controller);
+      _controller = controller;
+
+      // Find inputs by name and store references
+      _speakInput = controller.findInput<bool>('Speak');
+      _sAndOInput = controller.findInput<bool>('S&O');
+      _thoughtInput = controller.findInput<bool>('Thought');
+      _hiDoremonInput = controller.findInput<bool>('HiDoremon');
+      _sunnahSmileInput = controller.findInput<bool>('SunnahSmile');
+    }
   }
+
+  // Functions to trigger inputs
+  void triggerSpeak() => _speakInput?.value = true;
+  void triggerSAndO() => _sAndOInput?.value = true;
+  void triggerThought() => _thoughtInput?.value = true;
+  void triggerHiDoremon() => _hiDoremonInput?.value = true;
+  void triggerSunnahSmile() => _sunnahSmileInput?.value = true;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:rive/rive.dart';
-
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -15,10 +13,40 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: RiveAnimation.asset(
-          'assets/rive/copybilai.riv',
-          fit: BoxFit.cover,
-          onInit: controller.onRiveInit,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              RiveAnimation.asset(
+                'assets/rive/copybilai.riv',
+                fit: BoxFit.contain,
+                onInit: controller.onRiveInit,
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: controller.triggerSpeak,
+                    child: const Text('Trigger Speak'),
+                  ),
+                  ElevatedButton(
+                    onPressed: controller.triggerSAndO,
+                    child: const Text('Trigger S&O'),
+                  ),
+                  ElevatedButton(
+                    onPressed: controller.triggerThought,
+                    child: const Text('Trigger Thought'),
+                  ),
+                  ElevatedButton(
+                    onPressed: controller.triggerHiDoremon,
+                    child: const Text('Trigger HiDoremon'),
+                  ),
+                  ElevatedButton(
+                    onPressed: controller.triggerSunnahSmile,
+                    child: const Text('Trigger SunnahSmile'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
